0ce48f3514db1f9c7cefb441e090796393af1ea5
Amira Nazri <anazri@student.wethinkcode.co.za>
2019-07-23 13:11:18 +0200

    Third Commit - Libft compilation error fixed.

--- get_next_line.c
+++ get_next_line.c
@@ -5,58 +5,59 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: anazri <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/07 16:36:38 by anazri            #+#    #+#             */
-/*   Updated: 2019/07/07 16:48:02 by anazri           ###   ########.fr       */
+/*   Created: 2019/07/17 13:41:41 by anazri            #+#    #+#             */
+/*   Updated: 2019/07/17 13:42:57 by anazri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int		ft_lire(const int fd, char **line)
+static int	ft_lire(const int fd, char **line)
 {
 	int		con;
 	char	*buf;
-	char	*temp;
+	char	*tmp;
 
 	if (!(buf = ft_strnew(BUFF_SIZE)))
 		return (-1);
 	con = read(fd, buf, BUFF_SIZE);
 	if (con > 0)
 	{
-		if (!(temp = ft_strjoin(*line, buf)))
+		buf[con] = '\0';
+		if (!(tmp = ft_strjoin(*line, buf)))
 			return (-1);
-		ft_strdel(line);
-		*line = temp;
+		free(*line);
+		*line = tmp;
+		free(buf);
 	}
-	ft_strdel(&buf);
+	if (con == 0)
+		free(buf);
 	return (con);
 }
 
-int				get_next_line(const int fd, char **line)
+int			get_next_line(const int fd, char **line)
 {
 	int			con;
-	char		*str;
-	static char	*temp;
+	static char	*buf;
+	char		*tmp;
 
-	if ((!(temp = ft_strnew(BUFF_SIZE)) || fd < 0 || BUFF_SIZE == 0))
+	if (((!buf && (!(buf = ft_strnew(BUFF_SIZE)))) || fd < 0 || BUFF_SIZE <= 0))
 		return (-1);
-	str = NULL;
-	while (str == NULL)
+	tmp = ft_strchr(buf, 10);
+	while (!tmp)
 	{
-		con = ft_lire(fd, line);
+		con = ft_lire(fd, &buf);
+		if (con == 0 && !ft_strlen(buf))
+			return (0);
 		if (con == 0)
-		{
-			if (!ft_strlen(temp))
-				return (0);
-			ft_strcat(temp, "\n");
-		}
+			ft_strcat(buf, "\n");
 		if (con < 0)
 			return (-1);
 		else
-			str = ft_strchr(temp, '\n');
+			tmp = ft_strchr(buf, 10);
 	}
-	if (!(*line = ft_strsub(temp, 0, ft_strlen(temp) - ft_strlen(str))))
+	if (!(*line = ft_strsub(buf, 0, ft_strlen(buf) - ft_strlen(tmp))))
 		return (-1);
-	ft_strcpy(temp, str + 1);
+	ft_strcpy(buf, tmp + 1);
 	return (1);
 }
